edbc9a1129a48e04c3b6e00e7fa9d33595c3439e
Bermond Jon Batistiana <berjondev@gmail.com>
2020-10-03 11:46:38 -0700

    Heroku setup

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const noteController = require("./Controllers/noteController");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
